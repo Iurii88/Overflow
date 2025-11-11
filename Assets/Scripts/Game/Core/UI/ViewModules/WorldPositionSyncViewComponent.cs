96ce459ae30de802bc81df074004cd0aaf0be06d
Iurii <yuriy.lisenkov@gmail.com>
2025-11-11 20:17:33 +0300

    - improved sync position view component

--- Assets/Scripts/Game/Core/UI/ViewModules/WorldPositionSyncViewComponent.cs
+++ Assets/Scripts/Game/Core/UI/ViewModules/WorldPositionSyncViewComponent.cs
@@ -10,7 +10,10 @@ namespace Game.Core.UI.ViewModules
         public BlackboardViewParameter<Entity> entity;
 
         [SerializeField]
-        private Vector3 worldOffset = new(0f, 1f, 0f);
+        private Vector2 worldOffset = Vector2.zero;
+
+        private RectTransform m_rectTransform;
+        private Camera m_camera;
 
         protected override void Subscribe()
         {
@@ -19,6 +22,13 @@ namespace Game.Core.UI.ViewModules
 
         private void EntityOnOnVariableChanged(BlackboardVariable<Entity> entityVariable)
         {
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            m_rectTransform = GetComponent<RectTransform>();
+            m_camera = Camera.main;
         }
 
         private void LateUpdate()
@@ -38,9 +48,27 @@ namespace Game.Core.UI.ViewModules
             if (entityTransform == null)
                 return;
 
-            var screenPoint = Camera.main.WorldToScreenPoint(entityTransform.position + worldOffset);
-            var rectTransform = GetComponent<RectTransform>();
-            rectTransform.anchoredPosition = screenPoint;
+            if (m_camera == null)
+                return;
+
+            var worldPosition = (Vector2)entityTransform.position + worldOffset;
+            var screenPosition = m_camera.WorldToScreenPoint(worldPosition);
+
+            var parentRect = m_rectTransform.parent as RectTransform;
+            if (parentRect == null)
+            {
+                m_rectTransform.position = worldPosition;
+                return;
+            }
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                parentRect,
+                screenPosition,
+                null,
+                out var localPoint
+            );
+
+            m_rectTransform.anchoredPosition = localPoint;
         }
     }
 }
