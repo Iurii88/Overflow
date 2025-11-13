3af0cd12e2ba7c018f69a6542790d41c7fad28f4
Iurii <yuriy.lisenkov@gmail.com>
2025-11-14 02:36:43 +0300

    - destroy entities fixed

--- Assets/Scripts/Game/Features/Bootstraps/EcsBootstrap.cs
+++ Assets/Scripts/Game/Features/Bootstraps/EcsBootstrap.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using Game.Core.Factories;
 using Game.Core.Initialization;
+using Game.Core.Logging;
 using Game.Core.Reflection.Attributes;
 using UnsafeEcs.Core.Bootstrap;
 using UnsafeEcs.Core.Worlds;
@@ -12,14 +13,44 @@ using VContainer;
 namespace Game.Features.Bootstraps
 {
     [AutoRegister]
-    public class EcsBootstrap : IAsyncLoader, IDisposable
+    public class EcsBootstrap : IAsyncLoader, IUniTaskAsyncDisposable
     {
         [Inject]
         private readonly IObjectResolver m_resolver;
 
-        public void Dispose()
+        [Inject]
+        private IEntityFactory m_entityFactory;
+
+        public async UniTask DisposeAsync()
         {
+            await DestroyAllEntitiesAsync();
             WorldManager.Destroy();
+        }
+
+        private async UniTask DestroyAllEntitiesAsync()
+        {
+            var world = WorldManager.Worlds[0];
+            if (world == null)
+            {
+                GameLogger.Warning("[EcsBootstrap] No world found during disposal");
+                return;
+            }
+
+            var query = world.EntityManager.CreateQuery();
+            var allEntities = query.Fetch();
+
+            if (allEntities.Length == 0)
+            {
+                GameLogger.Log("[EcsBootstrap] No entities to destroy");
+                return;
+            }
+
+            GameLogger.Log($"[EcsBootstrap] Destroying {allEntities.Length} entities before world cleanup");
+
+            foreach (var entity in allEntities)
+                await m_entityFactory.DestroyEntityAsync(world.entityManagerWrapper, entity);
+
+            GameLogger.Log("[EcsBootstrap] All entities destroyed successfully");
         }
 
         public UniTask LoadAsync(CancellationToken cancellationToken)
